f6c647d9cc281c45a8cd1c4a390510a3feae97e8
grant <grntwalton@gmail.com>
2022-03-31 12:42:52 -0700

    get session user route working

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -23,6 +23,15 @@ router.post('/', asyncHandler(async (req, res, next) => {
     return res.json({ user });
 }))
 
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if(user) {
+        return res.json({
+            user: user.toSafeObject()
+        })
+    } else return res.json({})
+})
+
 router.delete('/', (_req, res) => {
     res.clearCookie('token')
     return res.json({message: 'success'});
