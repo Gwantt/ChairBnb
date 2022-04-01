e397515c8b73865a11dfbd1a3097f16abf5d70a4
grant <grntwalton@gmail.com>
2022-03-31 20:52:17 -0700

    sign up form complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from './store/session'
+import SingupFormPage from './components/SignUpForm';
+
 
 function App() {
+  const dispath = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => {
+    dispath(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  })
   return (
     <Switch>
+      <Route exact path='/'>
+        <h1>Home Page</h1>
+      </Route>
       <Route path='/login'>
         <LoginFormPage />
+      </Route>
+      <Route path='/signup'>
+        <SingupFormPage />
       </Route>
     </Switch>
   );
