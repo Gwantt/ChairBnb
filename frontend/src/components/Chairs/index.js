53bfc375adb06999cf2aa56c0809fae632f289d5
grant <grntwalton@gmail.com>
2022-04-02 20:18:34 -0700

    animated the chairs and styled them

--- frontend/src/components/Chairs/index.js
+++ frontend/src/components/Chairs/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 // import { getChairs } from '../../store/chairs';
 import * as chairActions from '../../store/chairs'
 import './chair.css'
+import { animate, motion } from 'framer-motion';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { Redirect } from 'react-router-dom';
 // import * as sessionActions from '../../store/session';
@@ -17,16 +18,9 @@ const Chairs = () => {
         dispatch(chairActions.getChairs())
     }, [dispatch])
 
-    // const chairs = useSelector(state => state.chair.chairs.map(chairId => state.chair[chairId]))
     const chairs = useSelector(state => state.chair);
 
-
     const chairData = Object.values(chairs)
-    // console.log(chairData)
-
-    // chairData.forEach(chair => {
-    //     console.log('Chair ', chair.image1)
-    // })
 
     console.log('Data -->',);
     console.log('chairs -->', chairs)
@@ -35,11 +29,11 @@ const Chairs = () => {
             <h1>Chair Listings</h1>
             <div className='chairCard'>
                 {chairData.map((chair, idx) => (
-                    <div className='chairDiv' key={idx}>
+                    <motion.div className='chairDiv' key={idx} initial={{opacity: 0, translateY: 50}} animate={{opacity: 1, translateY: 0}} transition={{duration: 1.5}}>
                         <img src={chair.image1} width='250px' height='250px' />
                         <p>${chair.price} / night</p>
                         <p>{chair.name}</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </>
