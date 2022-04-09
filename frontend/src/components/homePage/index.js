947441c3bbc0392a1aa93ae4ceef8c768d03d4a0
grant <grntwalton@gmail.com>
2022-04-09 16:12:25 -0700

    fixed homepage animation;

--- frontend/src/components/homePage/index.js
+++ frontend/src/components/homePage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as chairActions from '../../store/chairs'
 import './home.css'
+import { motion } from 'framer-motion';
 // import '../../Vector 1.png'
 const HomePage = () => {
     const dispatch = useDispatch();
@@ -19,11 +20,11 @@ const HomePage = () => {
             {/* <h1>Welcome To ChairBnb!</h1> */}
             <div className='homeCard'>
                 {chairData.map((chair, idx) => (
-                    <a href={`/chairs/${chair.id}`} className='homeDiv grow' key={idx}>
+                    <motion.a href={`/chairs/${chair.id}`} className='homeDiv' key={idx} key={idx} initial={{opacity: 0, translateY: 50}} animate={{opacity: 1, translateY: 0}} transition={{duration: 1.5}}>
                         <img src={chair.image1} width='250px' height='250px'></img>
                         <p>{chair.name}</p>
                         <p>${chair.price} / night</p>
-                    </a>
+                    </motion.a>
                 ))}
             </div>
         </>
