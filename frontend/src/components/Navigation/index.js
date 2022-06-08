31a78294209dec1f2fabb50198b6e9c4e8c8d41a
grant <grntwalton@gmail.com>
2022-06-08 14:38:35 -0700

    css for search bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import './Navigation.css'
 import ProfileButton from './ProfileButton';
 import logo from '../../images/logo.png'
 import DemoUser from '../DemoUser';
-import { motion } from 'framer-motion';
+import { motion, transform } from 'framer-motion';
 import * as searchActions from '../../store/search'
 
 // import './Navgation.css'
@@ -33,7 +33,7 @@ function Navigation({ isLoaded }) {
             <>
                 <NavLink activeClassName='active' className='aButton' to='/login'>Log In</NavLink>
                 <NavLink activeClassName='active' className='aButton' to='/signup'>Sign Up</NavLink>
-                <NavLink activeClassName='active' className='aButton' to='/chairs' >Chair Listings</NavLink>
+                <NavLink activeClassName='active' className='aButton' to='/chairs' onClick={() => searchActions.searchThunk({ searchParams: '' })}>Chair Listings</NavLink>
                 <DemoUser />
             </>
         )
@@ -46,6 +46,7 @@ function Navigation({ isLoaded }) {
             searchParams: searchContent
         }
         //dispatch search thunk using search params
+
         dispatch(searchActions.searchThunk(payload))
         //load the search results on the page instead of everything else
 
@@ -53,8 +54,8 @@ function Navigation({ isLoaded }) {
 
     return (
         <motion.nav
-            // animate={open ? 'open' : 'closed'}
-            // variants={variants}
+        // animate={open ? 'open' : 'closed'}
+        // variants={variants}
         >
             <li>
                 <NavLink activeClassName='active' className='aButton' exact to='/'>Home</NavLink>
@@ -66,16 +67,16 @@ function Navigation({ isLoaded }) {
                     <img className='logo' src={logo} alt='ChairBnb' />
                 </div>
             </li>
-            <li>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type='text'
-                        value={searchContent}
-                        placeholder='Search For Chairs'
-                        onChange={e => setSearchContent(e.target.value)}
-                    />
-                </form>
-            </li>
+            <form onSubmit={handleSubmit} style={{position:'absolute', transform:'translate(99em, -6.5em)'}}>
+                <input
+                    style={{height: '25px'}}
+                    className='formItem'
+                    type='text'
+                    value={searchContent}
+                    placeholder='Search For Chairs'
+                    onChange={e => setSearchContent(e.target.value)}
+                />
+            </form>
         </motion.nav>
     )
 }
